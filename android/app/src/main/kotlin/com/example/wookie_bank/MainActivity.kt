b57213f6b499d3582cf06134b9dd62de62aecdfb
Evgeny Kot <bunopus@gmail.com>
2020-02-16 18:27:51 +0300

    2. Receive message from native

--- android/app/src/main/kotlin/com/example/wookie_bank/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/wookie_bank/MainActivity.kt
@@ -7,6 +7,8 @@ import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
+import java.util.Arrays
+
 
 class MainActivity : FlutterActivity() {
     private val CHANNEL = "wookie.bank/vi"
@@ -17,9 +19,20 @@ class MainActivity : FlutterActivity() {
 
         flutterEngine.getDartExecutor().setMessageHandler(CHANNEL) { message, reply ->
             message?.order(ByteOrder.nativeOrder())
-            val x = message?.double
-            val n = message?.int
+
+            val x:Double = message?.double ?: 0.0
+            val n:Int = message?.int ?: 0
+
             Log.i("FLUTTER", "Received: $x and $n")
+
+            val message2 = ByteBuffer.allocateDirect(12)
+            message2.putDouble(x)
+            message2.putInt(n)
+            
+            flutterEngine.getDartExecutor().send(CHANNEL, message2) { _ ->
+                Log.i("FLUTTER", "Sent $x and $n")                
+            }
+
             reply?.reply(null)
         }
     }
--- lib/account.dart
+++ lib/account.dart
@@ -4,8 +4,36 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class AccountPage extends StatelessWidget {
+class AccountPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AccountPageState();
+  }
+}
+
+class _AccountPageState extends State<AccountPage> {
   static const CHANNEL_NAME = 'wookie.bank/vi';
+  String nativeMessage = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _listenChannel();
+  }
+
+  void _listenChannel() {
+    ServicesBinding.instance.defaultBinaryMessenger
+        .setMessageHandler(CHANNEL_NAME, (ByteData message) async {
+      final double x = message.getFloat64(0);
+      final int n = message.getInt32(8);
+
+      setState(() {
+        nativeMessage = "By the way: x = $x and n = $n";
+      });
+
+      return null;
+    });
+  }
 
   Future<Null> _sendDataToNative() async {
     final WriteBuffer buffer = WriteBuffer()
@@ -36,6 +64,14 @@ class AccountPage extends StatelessWidget {
                 child: Text("Connect"),
                 onPressed: _sendDataToNative,
               )),
+          Padding(
+            child: Center(
+                child: Text(
+              nativeMessage,
+              style: TextStyle(fontSize: 15),
+            )),
+            padding: EdgeInsets.only(bottom: 20),
+          ),
           Image(
               image: AssetImage("assets/wookie_head.png"),
               fit: BoxFit.scaleDown,
